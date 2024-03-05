39a0bd8e63fa23355a8928b0e98f85be7ad7f954
Nguyễn Hữu Thuật <105630903+huuthuatnguyen0911@users.noreply.github.com>
2024-03-05 16:43:08 +0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,11 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: "globalThis",
-    "process.env": {},
-  },
+  // define: {
+  //   global: "globalThis",
+  //   "process.env": {},
+  // },
+   build:{
+    outDir: "build"
+  }
 });
